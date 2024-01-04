dcb2d32829a82614f99c8dd23eb036fa3b5c3fa8
MargaretTechworld <margarettechworld@gmail.com>
2024-01-04 00:22:31 +0000

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_25_160044) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_04_000257) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_25_160044) do
     t.datetime "updated_at", null: false
     t.bigint "creator_id"
     t.string "name"
+    t.string "photo"
     t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
